94d0825c60d6ef61096a82a5fedc9d9217d5f6d8
Nick Dambrosio <ndambro303@gmail.com>
2018-07-20 11:45:58 -0600

    Clean up arrow navigation function

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 let slideIndex = 1;
-showSlides(slideIndex)
+showSlides(slideIndex);
 
-let plusSlides = (n) => showSlides(slideIndex += n);
+let plusSlides = (currentSlide) => showSlides(slideIndex += currentSlide);
 
-function showSlides(n){
+function showSlides(currentSlide){
   let slides = document.querySelectorAll('.hero-section-literal');
 
 
-  if(n > slides.length){
+  if(currentSlide > slides.length){
     slideIndex = 1
   }
 
-  if(n < 1){
+  if(currentSlide < 1){
     slideIndex = slides.length;
-    console.log(n);
   }
 
   for(let i = 0; i < slides.length; i++){
     slides[i].style.display = "none";
-    console.log(n);
   }
-  slides[slideIndex - 1].style.display = "block"
-}
+  slides[slideIndex - 1].style.display = "block";
+};
 
 showSlides(slideIndex);
